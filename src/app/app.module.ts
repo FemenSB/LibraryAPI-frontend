939c55a571b31b8bfa791ae732e1a10e5d1b86cc
FemenSB <felipe_sborsato@hotmail.com>
2022-06-30 19:44:34 -0300

    RouterModule and NavBar
    Routes are being handled by RouterModule. A status 404 error component is displayed on route **.
    
    NavBar is halfway implemented, being rendered with no anchors.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
 import { BookThumbnailComponent } from './books/book-thumbnail.component';
 import { StarsRating } from './books/stars-rating/stars.component';
 import { BookList } from './books/book-list.component';
 import { MainList } from './books/main-list.component';
-import { FormsModule } from '@angular/forms';
+import { Status404 } from './status-404.component';
+import { NavBar } from './nav-bar.component';
 
 @NgModule({
   declarations: [
@@ -15,10 +19,23 @@ import { FormsModule } from '@angular/forms';
     StarsRating,
     BookList,
     MainList,
+    Status404,
+    NavBar,
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'books', pathMatch: 'full'
+      },
+      {
+        path: 'books', component: MainList
+      },
+      {
+        path: '**', component: Status404
+      },
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
